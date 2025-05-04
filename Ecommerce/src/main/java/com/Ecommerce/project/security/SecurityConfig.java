cd8ee96fa3c21ab71a96a3e1ea5b0b1b1143dbf4
Savindu_Rashmika <savindurashmika2000@gmail.com>
2025-05-04 17:19:29 +0530

    Update SecurityConfig.java
    add custome configaratiton

--- Ecommerce/src/main/java/com/Ecommerce/project/security/SecurityConfig.java
+++ Ecommerce/src/main/java/com/Ecommerce/project/security/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.secure.notes.security;
+package com.Ecommerce.project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,9 +15,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        // http.formLogin(withDefaults());
+        http.authorizeHttpRequests((requests) ->
+                requests
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/hellow").denyAll()
+                        .anyRequest().authenticated());
+         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
